30ac6e4767d7931bef038281ce6789342687f934
TheRealRhyle <pwelch01@tampabay.rr.com>
2018-12-20 00:55:18 -0500

    Beginning link identification, and created user database.

--- build_command_table.py
+++ build_command_table.py
@@ -5,6 +5,24 @@ global c
 conn = sqlite3.connect("dxchatbot.db")
 c = conn.cursor()
 
+# c.execute("""create table users (
+#                 uname text,
+#                 status text)""")
+# conn.commit()
+
+# c.execute("""insert into users values ('Zangeru13','fots')""")
+
+
+# c.execute("""update users
+#             set status = ''
+#             where uname = 'spookeriffic'""")
+
+
+# c.execute("""delete from users where uname = 'orphenadler' and status = 'user'""")
+# conn.commit()
+
+ulist = c.execute("""select * from users""").fetchall()
+print(ulist)
 # c.execute('drop table commands')
 # conn.commit()
 #
@@ -13,13 +31,22 @@ c = conn.cursor()
 #                 target text,
 #                 action text)""")
 # conn.commit()
-#
-#
-# c.execute("""insert into commands values ('!help', '',' the current commands are !insult, !sso, !so, !permit. If I do """ \
+# #
+# #
+# c.execute("""insert into commands values ('!help', '',' the current commands are !help. If I do """ \
 #     """not respond to an issued command it is because you are not in my preferred list of users.')""")
 # conn.commit()
 
-print(c.execute('select * from commands').fetchall())
+
+tab = c.execute('select * from commands').fetchall()
+print(len(tab))
+for i in range(len(tab)):
+    print(tab[i])
+#
+# c.execute("""update commands
+#     set action = 'Find me on twitter to get stream updates and notifications when I go live! @dxstreaming twitter.com/dxstreaming'
+#     where ex_command = '!twitter'""")
+# conn.commit()
 
 # c.execute("""update commands
 #                 set action = ' this command is being worked on.  there are currently no user triggerable commands other than !help (or are there?)'
